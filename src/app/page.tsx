8f8d1d597734ef66e5414700526e9a9a644d3d91
KavanGamii <kavangami13@gmail.com>
2026-04-25 12:07:50 +0530

    fix: correct getSitePageById import name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { getFeaturedProjects, getConfig, getPageById } from "@/lib/db";
+import { getFeaturedProjects, getConfig, getSitePageById } from "@/lib/db";
 import HomeContent from "./HomeContent";
 
 export default async function HomePage() {
   const projects = await getFeaturedProjects();
   const config = await getConfig();
-  const pageData = await getPageById("home");
+  const pageData = await getSitePageById("home");
   
   return <HomeContent projects={projects} config={config} pageData={pageData} />;
 }
